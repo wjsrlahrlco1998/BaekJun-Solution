479e046ae348cf8a74ef57e73ccba650780f045c
ParkJiSeung <wjsrlahrlco@daum.net>
2022-04-16 00:09:46 +0900

    Update : BOJ_20055.py

--- 골드문제/BOJ_20055.py
+++ 골드문제/BOJ_20055.py
@@ -1,54 +1,46 @@
-import time
+from collections import deque
 
 lenOfBelt, maxBrokenCount = map(int, input().split())
 AList = list(map(int, input().split()))
 
-Belt = [[i + 1, 0, 0] for i in range(lenOfBelt * 2)]
+# 벨트번호, 로봇유무, 내구도
+Belt = [[i + 1, 0, AList[i]] for i in range(lenOfBelt * 2)]
+Belt = deque(Belt)
 
+# 진행단계
 level = 0
+# 내구성이 0인 칸의 개수
 brokenCount = 0
 
-for i in range(lenOfBelt * 2):
-    Belt[i][2] = AList[i]
+def outRobot():
+    if Belt[lenOfBelt - 1][1] == 1:
+        Belt[lenOfBelt - 1][1] = 0
 
 while maxBrokenCount > brokenCount:
     level += 1
-    brokenCount = 0
-    print(level,Belt)
+
+    outRobot()
 
     # 1. 벨트회전
-    Belt.insert(0, Belt[-1])
-    del Belt[-1]
-    # 로봇 내리기
-    if Belt[lenOfBelt - 1][1] == 1:
-        Belt[lenOfBelt - 1][1] = 0
-    print(level,Belt)
+    Belt.rotate(1)
+
+    outRobot()
 
     # 2. 로봇의 이동
-    yesMove = []
     for i in range(lenOfBelt - 2, -1, -1):
         if Belt[i][1] == 1 and Belt[i + 1][1] == 0 and Belt[i + 1][2] >= 1:
-            yesMove.append(i)
-    for i in yesMove:
-        Belt[i][1] = 0
-        Belt[i+1][1] = 1
-        Belt[i+1][2] -= 1
-        # 로봇 내리기
-        if Belt[lenOfBelt - 1][1] == 1:
-            Belt[lenOfBelt - 1][1] = 0
-    print(level,Belt)
+            Belt[i][1] = 0
+            Belt[i + 1][1] = 1
+            Belt[i + 1][2] -= 1
+            outRobot()
+            if Belt[i+1][2] == 0:
+                brokenCount += 1
 
     # 3. 로봇 올리기
     if Belt[0][2] != 0 and Belt[0][1] == 0:
         Belt[0][1] = 1
         Belt[0][2] -= 1
-    print(level, Belt)
-
-    # 4. brokenCount 검사
-    for i in range(lenOfBelt * 2):
-        if Belt[i][2] == 0:
+        if Belt[0][2] == 0:
             brokenCount += 1
-    print('='*70)
-    #time.sleep(1)
+
 print(level)
-print(brokenCount)
